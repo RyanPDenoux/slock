76073d06a511a85167f63296263197e23f1972fc
Arnas Udovicius <zordsdavini@gmail.com>
2019-11-26 16:16:15 +0200

    Read colors from Xresources

--- config.h
+++ config.h
@@ -9,5 +9,15 @@ static const char *colorname[NUMCOLS] = {
 	[CAPS] =   "red",       /* CapsLock on */
 };
 
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+	{ "color0", STRING, &colorname[INIT] },
+	{ "color4", STRING, &colorname[INPUT] },
+	{ "color1", STRING, &colorname[FAILED] },
+	{ "color3", STRING, &colorname[CAPS] },
+};
+
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
